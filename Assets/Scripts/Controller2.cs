c7ae8ab1f8b2526a9eadaf57619b1368ee4c05d9
MalcolmFFarrugia <malcolm.fenech.a100179@mcast.edu.mt>
2017-12-11 17:32:38 +0100

    mouse player done

--- Assets/Scripts/Controller2.cs
+++ Assets/Scripts/Controller2.cs
@@ -5,27 +5,21 @@ using UnityEngine;
 public class Controller2 : MonoBehaviour
 {
     public GameObject Player2;
-    public float boundY = 3.35f;
-
-    public float speed = 5f;
-
-
-
+    public float boundY = 5.5f;
+    public Rigidbody2D rb2;
 
 
     // Use this for initialization
     void Start() {
-
+        rb2 = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetMouseButtonDown(0))
-        {
 
-        }
-
-       
+        float mousePos = (Input.mousePosition.y / Screen.height * 13f) - 6.5f;
+        Vector3 Player2pos = new Vector3(12f, mousePos, this.transform.position.x);
+        this.transform.position = Player2pos;
 
         var position = transform.position;
         if (position.y > boundY)
